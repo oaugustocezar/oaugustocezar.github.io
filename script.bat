25fd919baaa41ce9028a9ae5158daf00879ae8f6
Augusto Cézar <augusto.c.ferreira@hotmail.com>
2020-09-14 09:31:22 -0300

    atualizacao do scritp

--- script.bat
+++ script.bat
@@ -1,4 +1,9 @@
+@echo off
+echo "Script para realizar commits no github"
+set /p commitMessage=O que voce esta commitando?
+pause
 git status
 git add ./
-git commit -m "alterações com DOM"
+pause
+git commit -m "%commitMessage%"
 git push origin master
