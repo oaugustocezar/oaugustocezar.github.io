cbf1969eb6ae4ac54615c34e83fc0d97328c4dee
Augusto Cézar <augusto.c.ferreira@hotmail.com>
2020-09-15 11:01:45 -0300

    adicionando o js em pasta separada

---
+++ js/myscript.js
@@ -0,0 +1,47 @@
+function responsive_menu (){
+            var button = document.querySelector(".menu-icon");
+        var site = document.querySelector(".content");
+        var lista = document.querySelector(".header-container");
+        var header = document.querySelector(".header");
+        var close = document.querySelector(".close-icon");
+        
+             button.addEventListener('click',function(){
+
+                site.className = 'esconder';
+                lista.className = 'header-container-show';
+                header.className ='header_new'
+                close.addEventListener('click',function(){
+                    lista.className = 'header-container';
+                    header.className = 'header';
+                    site.className = 'content';
+                });
+                
+
+        });
+}
+
+function search_bar(){
+
+    var header = document.querySelector(".header");
+
+    var search = document.querySelector(".search-icon");
+
+    search.addEventListener('click', function(){
+
+        header.className = 'search-show';
+
+        close.addEventListener('click',function(){
+
+            header.className = 'header';
+
+        });
+
+
+
+    });
+
+
+}
+        
+
+       
--- script.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo "Script para realizar commits no github"
-set /p commitMessage=O que voce esta commitando?
-pause
-git status
-git add ./
-pause
-git commit -m "%commitMessage%"
-git push origin master
