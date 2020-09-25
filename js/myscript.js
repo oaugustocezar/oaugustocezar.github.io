9af8920fce8c43d7425ac0c9396d6d16da35c9a2
Andre Valario <andrevalario13@gmail.com>
2020-09-25 16:56:30 -0300

    atualizando erros conforme feedback

--- js/myscript.js
+++ js/myscript.js
@@ -54,39 +54,65 @@ function setUserValues(){
     var passwd = document.getElementById('passwd');
     var confpasswd = document.getElementById('confpasswd');  
 
+    var verify_user = document.getElementById('username-verify');
+    var verify_passwd = document.getElementById("passwd-verify");
+    var verify_confpasswd = document.getElementById("confpasswd-verify");
+    var validate_username = "";
+    var validate_passwd ="";
+
+    username.addEventListener("keyup", function(){
+
+        if(username.value == ''){
+
+            verify_user.innerHTML = "Email deve ser preenchido";           
+            
+        }else if (validacaoEmail(username)){
+            validate_username ="ok";
+            verify_user.innerHTML = "";         
+            
+
+        }else{
+
+            verify_user.innerHTML = "E-mail inválido";
+
+                       
+            
+        }
+
+
+    });
+
+    passwd.addEventListener("keyup",function(){
+
+        if(passwd.value == ''){
+            verify_passwd.innerHTML = "Senha deve ser preenchida";   
+            verify_confpasswd.innerHTML = "";                    
+        }
+        if(passwd.value.length < 4){
+            verify_passwd.innerHTML = "Senha inválida";
+            verify_confpasswd.innerHTML = "";
+        }else if(passwd.value == confpasswd.value){
+            validate_passwd = "ok";
+            verify_passwd.innerHTML = "";
+            verify_confpasswd.innerHTML = "";
+            
+            
+        }else if (passwd.value != confpasswd.value){
+            verify_confpasswd.innerHTML = "";
+            verify_passwd.innerHTML = "";
+        }else{
+            verify_confpasswd.innerHTML ="";
+
+        }
+
+
+    });
+
     
 
     form.addEventListener('click', function(){
 
 
-        if(username.value == ''){
-
-            alert("Email deve ser preenchido");
-            
-        }else if (username.value.length < 4){
-
-            alert("Email inválido");
-            
-
-        }else{
-
-            var validate_username ="ok";
-            
-        }
-
-        if(passwd.value == ''){
-            alert("Senha deve ser preenchida");
-        }else if(passwd.value.length < 4){
-            alert("Senha inválida");
-        }else if(passwd.value == confpasswd.value){
-            var validate_passwd = "ok";
-            
-        }else{
-            alert("senhas não coincidem");
-        }
-        
-        
-        
 
         if(validate_username == "ok" && validate_passwd == "ok"){
 
@@ -101,7 +127,7 @@ function setUserValues(){
 
                 if(r.status == 200){
                     alert("Cadastro realizado com sucesso");   
-                    open('login.html')                 
+                    open('login.html');                 
                 }
 
                 
@@ -110,7 +136,7 @@ function setUserValues(){
             })
             .catch(function(error){   
                 
-                alert(error);
+                console.log(error);
                 
                
             }); 
@@ -133,14 +159,68 @@ function setUserValues(){
     
 }
 
+function validacaoEmail(field) {
+    usuario = field.value.substring(0, field.value.indexOf("@"));
+    dominio = field.value.substring(field.value.indexOf("@")+ 1, field.value.length);
+    
+    if ((usuario.length >=1) &&
+        (dominio.length >=3) &&
+        (usuario.search("@")==-1) &&
+        (dominio.search("@")==-1) &&
+        (usuario.search(" ")==-1) &&
+        (dominio.search(" ")==-1) &&
+        (dominio.search(".")!=-1) &&
+        (dominio.indexOf(".") >=1)&&
+        (dominio.lastIndexOf(".") < dominio.length - 1)) {
+    
+    return true;
+    }
+    else{
+    
+    return false;
+    }
+    }
+
+
+
 function getUserValues(){
+
+
+
+    
 
 
     var form = document.getElementById('button_login');
     var username = document.getElementById('username');
     var passwd = document.getElementById('passwd');
+    var verify_user = document.getElementById('verify_username');
+    var verify_passwd = document.getElementById("verify_passwd");
 
-    form.addEventListener('click', function(e){   
+    username.addEventListener("keyup",function(){
+        if(validacaoEmail(username)){
+            verify_user.innerHTML="";            
+        }else{
+            verify_user.innerHTML="Não é um e-mail válido";
+
+        }             
+
+    });
+
+    passwd.addEventListener("keyup", function(){
+        if(passwd.value.length < 3){
+            verify_passwd.innerHTML = "Não é uma senha válida";
+        }else{
+
+            verify_passwd.innerHTML = "";
+
+        }
+
+    });
+
+
+
+
+    form.addEventListener('click', function(e){        
 
         
         
@@ -163,7 +243,7 @@ function getUserValues(){
             })
             .catch(function(error){   
                 
-                alert("Credenciais inválidas");
+                
                 
                
             }); 
@@ -223,58 +303,74 @@ function verify_login(){
 
 function search_api(){
 
+
+    var login = localStorage.getItem("login");
     var search = document.getElementById('search');
     var button = document.getElementById('button_api');
     var p = document.getElementById('result');
     
-    button.addEventListener('click', function(){
-        axios.get("https://calendarific.com/api/v2/holidays?&api_key=bb433f717e522421e7b553183371f2c27a83feae&country=BR&year="+search.value.substring(0,4))
-            .then(function(res){
-                console.log(res.data.response.holidays)
-                let docs = res.data.response.holidays;
-                var i =0;
-                var control=0;
-                for(i; i < docs.length; i++){
-                    
-                    if(docs[i].date.iso==search.value){
+
+    if(login == null){
+        var li = document.createElement('li');
+        li.innerHTML ="Necessário estar logado para acessar pesquisa. Faça agora o "+"<a href="+"login.html"+">Login</a>";
+        p.appendChild(li);
+
+
+    }else{
+        button.addEventListener('click', function(){
+            axios.get("https://calendarific.com/api/v2/holidays?&api_key=bb433f717e522421e7b553183371f2c27a83feae&country=BR&year="+search.value.substring(0,4))
+                .then(function(res){
+                    console.log(res.data.response.holidays)
+                    let docs = res.data.response.holidays;
+                    var i =0;
+                    var control=0;
+                    for(i; i < docs.length; i++){
                         
-
-                        var li = document.createElement('li');
-                        li.innerHTML = "Date: "+docs[i].date.iso+"<br>"+"Name: "+ docs[i].name+"<br>" + "Description: "+docs[i].description+"<br>"+ "Country: "+docs[i].country.name+"<br>";
-                                          
-                        p.appendChild(li);                         
+                        if(docs[i].date.iso==search.value){
+                            
+    
+                            var li = document.createElement('li');
+                            li.innerHTML = "Date: "+docs[i].date.iso+"<br>"+"Name: "+ docs[i].name+"<br>" + "Description: "+docs[i].description+"<br>"+ "Country: "+docs[i].country.name+"<br>";
+                                              
+                            p.appendChild(li);                         
+                            
+    
+    
+                        }else{
+    
+                            control ++;
                         
-
-
-                    }else{
-
-                        control ++;
-                    
-                    }  
-                    
-                    
-
-                    
+                        }  
+                        
+                        
+    
+                        
+                    }
+    
+                
+                if(control == i ){
+                    var li = document.createElement('li');
+                    li.innerHTML ="não encontrado";
+                                      
+                    p.appendChild(li);    
+    
                 }
+    
+                           
+                    
+                    
+                });
+    
+    
+    
+    
+        });
 
-            
-            if(control == i ){
-                var li = document.createElement('li');
-                li.innerHTML ="não encontrado";
-                                  
-                p.appendChild(li);    
+    }
 
-            }
-
-                       
-                
-                
-            });
-
-
-
-
-    });
+    
+    
+   
 
 
 
